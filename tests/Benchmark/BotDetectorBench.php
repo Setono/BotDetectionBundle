6b596517fb1615e3e6ade5c6d1e36f27e3806784
Joachim Løvgaard <joachim@loevgaard.dk>
2021-11-17 14:03:56 +0100

    Add benchmark of matomo

--- tests/Benchmark/BotDetectorBench.php
+++ tests/Benchmark/BotDetectorBench.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\BotDetectionBundle\Tests\Benchmark;
 
+use DeviceDetector\Parser\Bot as BotParser;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 use Setono\BotDetectionBundle\BotDetector\BotDetector;
@@ -23,6 +24,23 @@ final class BotDetectorBench
 
         foreach ($bots as $bot) {
             $detector->isBot($bot);
+        }
+    }
+
+    /**
+     * @Revs(1000)
+     * @Iterations(5)
+     */
+    public function bench_matomo(): void
+    {
+        $botParser = new BotParser();
+
+        $bots = require __DIR__ . '/../data/bots.php';
+
+        foreach ($bots as $bot) {
+            $botParser->setUserAgent($bot);
+            $botParser->discardDetails();
+            $botParser->parse();
         }
     }
 }
