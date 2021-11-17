3aaa77a09922c57039847b29b0812871b8abf458
Joachim Løvgaard <joachim@loevgaard.dk>
2021-11-17 11:20:04 +0100

    Add extension test

---
+++ tests/DependencyInjection/SetonoBotDetectionExtensionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\BotDetectionBundle\Tests\DependencyInjection;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Setono\BotDetectionBundle\BotDetector\BotDetectorInterface;
+use Setono\BotDetectionBundle\DependencyInjection\SetonoBotDetectionExtension;
+
+/**
+ * @covers \Setono\BotDetectionBundle\DependencyInjection\SetonoBotDetectionExtension
+ */
+final class SetonoBotDetectionExtensionTest extends AbstractExtensionTestCase
+{
+    protected function getContainerExtensions(): array
+    {
+        return [new SetonoBotDetectionExtension()];
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_services(): void
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasService(BotDetectorInterface::class);
+        $this->assertContainerBuilderHasService('setono_bot_detection.bot_detector.default');
+        $this->assertContainerBuilderHasAlias(BotDetectorInterface::class, 'setono_bot_detection.bot_detector.default');
+    }
+}
